e6f2fc0d422947e8f20c00d2bc73fa91e5cc6ffb
Alan Jackson <alan.jackson@sage.com>
2023-05-10 19:45:59 +0100

    simple sample

---
+++ Bitmasks/BitMaskEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitmasks
+{
+    internal enum BitMaskEnum
+    {
+        None = 0,
+        Bit1 = 1,
+        Bit2 = 2,
+        Bit3 = 4,
+        Bit4 = 8
+    }
+}
---
+++ Bitmasks/Program.cs
@@ -0,0 +1,56 @@
+﻿using Bitmasks;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int iMin = 0;
+            int iMax = 32;
+            do
+            {
+                Console.WriteLine($"Enter a number between {iMin} and {iMax}");
+                Console.WriteLine("Enter <exit> to exit or <loop> to see how addition works");
+
+                string? input = Console.ReadLine();
+                int iInput;
+
+                if (int.TryParse(input, out iInput))
+                {
+                    if (iInput < iMin || iInput > iMax)
+                    {
+                        continue;
+                    }
+
+                    Console.WriteLine($"{iInput} & {(int)BitMaskEnum.Bit1} = {(iInput & (int)BitMaskEnum.Bit1) == (int)BitMaskEnum.Bit1}");
+                    Console.WriteLine($"{iInput} & {(int)BitMaskEnum.Bit2} = {(iInput & (int)BitMaskEnum.Bit2) == (int)BitMaskEnum.Bit2}");
+                    Console.WriteLine($"{iInput} & {(int)BitMaskEnum.Bit3} = {(iInput & (int)BitMaskEnum.Bit3) == (int)BitMaskEnum.Bit3}");
+                    Console.WriteLine($"{iInput} & {(int)BitMaskEnum.Bit4} = {(iInput & (int)BitMaskEnum.Bit4) == (int)BitMaskEnum.Bit4}");
+                }
+                else if (input?.ToLower() == "loop")
+                {
+                    Console.WriteLine($"Starting at {iInput}");
+                    Console.WriteLine($"{iInput} |= {(int)BitMaskEnum.Bit1} = {iInput |= (int)BitMaskEnum.Bit1}");
+                    Console.WriteLine($"{iInput} |= {(int)BitMaskEnum.Bit2} = {iInput |= (int)BitMaskEnum.Bit2}");
+                    Console.WriteLine($"{iInput} |= {(int)BitMaskEnum.Bit3} = {iInput |= (int)BitMaskEnum.Bit3}");
+                    Console.WriteLine($"{iInput} |= {(int)BitMaskEnum.Bit4} = {iInput |= (int)BitMaskEnum.Bit4}");
+
+                    iInput = 0;
+                    Console.WriteLine($"Starting at {iInput}");
+                    Console.WriteLine($"{iInput} |= {(int)BitMaskEnum.Bit4} = {iInput |= (int)BitMaskEnum.Bit4}");
+                    Console.WriteLine($"{iInput} |= {(int)BitMaskEnum.Bit3} = {iInput |= (int)BitMaskEnum.Bit3}");
+                    Console.WriteLine($"{iInput} |= {(int)BitMaskEnum.Bit2} = {iInput |= (int)BitMaskEnum.Bit2}");
+                    Console.WriteLine($"{iInput} |= {(int)BitMaskEnum.Bit1} = {iInput |= (int)BitMaskEnum.Bit1}");
+                }
+                else if (input?.ToLower() == "exit")
+                {
+                    return;
+                }
+            } while (true);
+
+            return;
+        }
+    }
+}
